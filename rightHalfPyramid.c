e6690f113b66e7c204b49a3848f397f005e4d95f
Kalia Gouda <75370240+Kalia832@users.noreply.github.com>
2024-01-20 20:31:59 +0530

    Update rightHalfPyramid.c

--- rightHalfPyramid.c
+++ rightHalfPyramid.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n=5;
+    int i,j,n=5,k;
     for(i=0;i<n;i++)
     {
+        for(k=0;k<n-i;k++)
+            printf(" ");
         for(j=0;j<=i;j++)
             printf("*");
         printf("\n");
